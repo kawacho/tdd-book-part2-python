2e6c8769300929eb140eac4a2f0b634e244380de
kawacho <kawacho@gmail.com>
2018-07-20 01:59:55 +0900

    P.145 testMethod メソッドを定義
    空実装（pass）
    
    エラーがなく実行できるようになったが、print されるのが None, None で期待通りではない。

--- xunit.py
+++ xunit.py
@@ -2,6 +2,9 @@ class WasRun:
     def __init__(self, name):
         self.wasRun = None
 
+    def testMethod(self):
+        pass
+
 test = WasRun("testmethod")
 print(test.wasRun)
 test.testMethod()
